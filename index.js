3256c55ecb9a71abe4e543457b35da919b5c5b2a
onTheDL <daniel.laserna@protonmail.com>
2020-11-17 22:03:58 -0500

    Render search results

--- index.js
+++ index.js
@@ -7,12 +7,26 @@ const fetchData = async (query) => {
       s: query,
     },
   });
-  console.log(res.data);
+
+  if (res.data.Error) {
+    return []
+  }
+
+  return res.data.Search;
 };
 
 const input = document.querySelector("input");
 
-const onInput = (e) => {
-  fetchData(e.target.value)
+const onInput = async (e) => {
+  const movies = await fetchData(e.target.value)
+  
+  for (let movie of movies) {
+    const div = document.createElement('div')
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+    `
+    document.querySelector('#target').appendChild(div)
+  }
 };
 input.addEventListener("input", debounce(onInput, 500));
