e242b6cd3ee7143fa05d866febd8a7d7d22f35d3
onTheDL <daniel.laserna@protonmail.com>
2020-11-17 20:53:27 -0500

    Add fetch search input with delay

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hi there!');
+const API_KEY = "7bce51db";
+
+const fetchData = async (query) => {
+  const res = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: API_KEY,
+      s: query,
+    },
+  });
+  console.log(res.data);
+};
+
+const input = document.querySelector("input");
+
+let timeoutId;
+const onInput = (e) => {
+  /*
+  if(timeoutId) {
+    clearTimeout(timeouteId)
+  }
+  */
+
+  timeoutId && clearTimeout(timeoutId)
+  timeoutId = setTimeout(() => {
+    fetchData(e.target.value);
+  }, 500);
+};
+input.addEventListener("input", onInput);
