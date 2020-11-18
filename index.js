17a1f3089273e1f965c2d2bd1265ce74f87d94ac
onTheDL <daniel.laserna@protonmail.com>
2020-11-17 23:52:42 -0500

    Add search results in dropdown

--- index.js
+++ index.js
@@ -9,24 +9,52 @@ const fetchData = async (query) => {
   });
 
   if (res.data.Error) {
-    return []
+    return [];
   }
 
   return res.data.Search;
 };
 
-const input = document.querySelector("input");
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+  <label><b>Search for a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+const input = document.querySelector(".input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
 const onInput = async (e) => {
-  const movies = await fetchData(e.target.value)
-  
+  const movies = await fetchData(e.target.value);
+
+  dropdown.classList.add("is-active");
   for (let movie of movies) {
-    const div = document.createElement('div')
-    div.innerHTML = `
+    const option = document.createElement("a");
+    option.classList.add('dropdown-item')
+    option.innerHTML = `
       <img src="${movie.Poster}" />
-      <h1>${movie.Title}</h1>
-    `
-    document.querySelector('#target').appendChild(div)
+      ${movie.Title}
+    `;
+    resultsWrapper.appendChild(option);
   }
 };
 input.addEventListener("input", debounce(onInput, 500));
+
+/*
+<div class="dropdown is-active">
+        <input />
+        <div class="dropdown-menu">
+          <div class="dropdown-content">
+            <a class="dropdown-item">Movie 1</a>
+            <a class="dropdown-item">Movie 2</a>
+            <a class="dropdown-item">Movie 3</a>
+          </div>
+        </div>
+      </div>
+      <div id="target"></div>
+*/
