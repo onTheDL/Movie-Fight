8afca6cbc83643b91a0bbdf4a6a2c2c1d352db23
onTheDL <daniel.laserna@protonmail.com>
2020-11-19 22:11:01 -0500

    Add error handling and styling to show winner vs loser

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const autoCompleteConfig = {
     `;
   },
   inputValue(movie) {
-    return movie.Title
+    return movie.Title;
   },
   async fetchData(query) {
     const res = await axios.get("http://www.omdbapi.com/", {
@@ -19,83 +19,140 @@ const autoCompleteConfig = {
         s: query,
       },
     });
-  
+
     if (res.data.Error) {
       return [];
     }
     return res.data.Search;
   },
-}
+};
 
 // Left Side
 createAutoComplete({
   ...autoCompleteConfig,
 
-  root: document.querySelector('#left-autocomplete'),
+  root: document.querySelector("#left-autocomplete"),
 
   onOptionSelect(movie) {
     onMovieSelect(
       movie.imdbID,
-      document.querySelector('#left-summary'),
-      'left',
+      document.querySelector("#left-summary"),
+      "left"
     );
 
-    document.querySelector('.tutorial').classList.add('is-hidden');
+    document.querySelector(".tutorial").classList.add("is-hidden");
   },
-})
+});
 
 // Right Side
 createAutoComplete({
   ...autoCompleteConfig,
 
-  root: document.querySelector('#right-autocomplete'),
+  root: document.querySelector("#right-autocomplete"),
 
   onOptionSelect(movie) {
     onMovieSelect(
       movie.imdbID,
-      document.querySelector('#right-summary'),
-      'right',
+      document.querySelector("#right-summary"),
+      "right"
     );
-    document.querySelector('.tutorial').classList.add('is-hidden');
+    document.querySelector(".tutorial").classList.add("is-hidden");
   },
-})
+});
 
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movieID, synopsisTarget, side) => {
-  const res = await axios.get("http://www.omdbapi.com/", {
-    params: {
-      apikey: API_KEY,
-      i: movieID,
-    },
-  });
-  console.log(res.data);
+  try {
+    const res = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: API_KEY,
+        i: movieID,
+      },
+    });
+    console.log(res.data);
 
-  synopsisTarget.innerHTML = movieTemplate(res.data)
+    synopsisTarget.innerHTML = movieTemplate(res.data);
 
-  if (side === 'left') {
-    leftMovie = res.data
-  } else {
-    rightMovie = res.data
+    if (side === "left") {
+      leftMovie = res.data;
+    } else {
+      rightMovie = res.data;
+    }
+
+    if (leftMovie && rightMovie) {
+      runComparison();
+    }
   }
-
-  if (leftMovie && rightMovie) {
-    runComparison()
+  catch(err){
+    synopsisTarget.innerHTML = `Error fetching data. Please try another movie.`
+    console.error('Error in onMovieSelect() in index.js: ', err)
   }
 };
 
 const runComparison = () => {
-  console.log('Time for comparison');
-}
+  // ls = left side
+  const lsStats = document.querySelectorAll("#left-summary .notification");
 
+  // rs = right side
+  const rsStats = document.querySelectorAll("#right-summary .notification");
+
+  lsStats.forEach((leftStat, idx) => {
+    const rightStat = rsStats[idx];
+
+    const lsVal = parseInt(leftStat.dataset.value);
+    const rsVal = parseInt(rightStat.dataset.value);
+
+    if (rsVal > lsVal) {
+      leftStat.classList.remove("is-primary");
+      leftStat.classList.add("is-warning");
+    } else if (rsVal < lsVal) {
+      rightStat.classList.remove("is-primary");
+      rightStat.classList.add("is-warning");
+    } else {
+      leftStat.classList.remove("is-primary");
+      leftStat.classList.add("is-warning");
+
+      rightStat.classList.remove("is-primary");
+      rightStat.classList.add("is-warning");
+    }
+  });
+};
 
 const movieTemplate = (movieData) => {
-  const { Title, Poster, Genre, Plot, Awards, BoxOffice, Metascore, imdbRating, imdbVotes, } = movieData;
+  const {
+    Title,
+    Poster,
+    Genre,
+    Plot,
+    Awards,
+    BoxOffice,
+    Metascore,
+    imdbRating,
+    imdbVotes,
+  } = movieData;
+
+  // Comparision values
+  const dollars = parseInt(BoxOffice.replace(/\$/g, "").replace(/,/g, ""));
+
+  const metascoreVal = parseInt(Metascore);
+  const imdbRatingVal = parseFloat(imdbRating);
+  const imdbVotesVal = parseInt(imdbVotes.replace(/,/g, ""));
+
+  const awardsVal = Awards.split(" ").reduce((prev, word) => {
+    const value = parseInt(word);
+    if (isNaN(value)) {
+      return prev;
+    } else {
+      return prev + value;
+    }
+  }, 0);
+
   return `
     <article class="media">
       <figure class="media-left">
         <p class="image">
-        ${(Poster !== 'N/A' || !Poster) ? `<img src=${Poster} />`: ''}
+        ${Poster !== "N/A" || !Poster ? `<img src=${Poster} />` : ""}
         </p>
       </figure>
       <div class="media-content">
@@ -107,24 +164,24 @@ const movieTemplate = (movieData) => {
       </div>
     </article>
 
-    <article class="notification is-primary">
+    <article data-value=${awardsVal} class="notification is-primary">
       <p class="title">${Awards}</p>
       <p class="subtitle">Awards</p>
     </article>
 
-    <article class="notification is-primary">
-      <p class="title">${BoxOffice ? BoxOffice : 'N/A'}</p>
+    <article data-value=${dollars} class="notification is-primary">
+      <p class="title">${BoxOffice ? BoxOffice : "N/A"}</p>
       <p class="subtitle">Box Office</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${metascoreVal} class="notification is-primary">
       <p class="title">${Metascore}</p>
       <p class="subtitle">Metascore</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${imdbRatingVal} class="notification is-primary">
       <p class="title">${imdbRating}</p>
       <p class="subtitle">IMDB Rating</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${imdbVotesVal} class="notification is-primary">
       <p class="title">${imdbVotes}</p>
       <p class="subtitle">IMDB Votes</p>
     </article>
