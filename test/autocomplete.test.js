150e21bee1e049ea60936d33dbdcf3debeb0f00a
Daniel Laserna <daniel.laserna@protonmail.com>
2021-04-13 15:30:50 -0400

    Simulate fake DOM events for testing

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,19 +1,35 @@
-it("Shows an autocomplete", () => {
+// hook
+beforeEach(() => {
+  document.querySelector("#target").innerHTML = "";
   createAutoComplete({
     root: document.querySelector("#target"),
     fetchData() {
       return [
-        { Title: "Avengers" }, 
+        { Title: "Avengers" },
         { Title: "Not Avengers" },
         { Title: "Some other movie" },
       ];
     },
     renderOption(movie) {
-      return movie.Title
-    }
+      return movie.Title;
+    },
   });
+});
 
-  const dropdown = document.querySelector('.dropdown')
+it("Dropdown starts closed", () => {
+  const dropdown = document.querySelector(".dropdown");
 
-  expect(dropdown.className).not.to.include('is-active');
+  expect(dropdown.className).not.to.include("is-active");
+});
+
+it("After searching, dropdown opens up", () => {
+  // type something in input
+  const input = document.querySelector("input");
+  input.value = "avengers";
+  input.dispatchEvent(new Event('input'))
+
+  // check dropdown
+  const dropdown = document.querySelector(".dropdown");
+
+  expect(dropdown.className).to.include("is-active");
 });
