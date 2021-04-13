8313f06243e8553905219f29571fd2a14b22d72b
Daniel Laserna <daniel.laserna@protonmail.com>
2021-04-13 15:07:44 -0400

    Write DOM test assertions using chai

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -12,4 +12,8 @@ it("Shows an autocomplete", () => {
       return movie.Title
     }
   });
+
+  const dropdown = document.querySelector('.dropdown')
+
+  expect(dropdown.className).not.to.include('is-active');
 });
